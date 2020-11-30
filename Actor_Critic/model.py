72b451dd3eb3e9670f1b27a27bdcb5460c01c2fc
Kailash Nadkar <nadkar.k@husky.neu.edu>
2020-11-30 17:12:55 -0500

    critic net model

--- Actor_Critic/model.py
+++ Actor_Critic/model.py
@@ -127,4 +127,54 @@ class ActorNet(nn.Module):
         return actor_out
 
 
+class CriticNet(nn.Module):
+    def __init__(self, num_actions, frame_dim):
+        super(CriticNet, self).__init__()
 
+        # create a convolution net
+        self.conv1 = nn.Conv2d(in_channels=frame_dim[2], out_channels=FIRST_LAYER_OUT,
+                               kernel_size=FIRST_LAYER_KERNEL_SIZE, stride=FIRST_LAYER_STRIDE)
+
+        self.conv2 = nn.Conv2d(in_channels=FIRST_LAYER_OUT, out_channels=SECOND_LAYER_OUT,
+                               kernel_size=SECOND_LAYER_KERNEL_SIZE, stride=SECOND_LAYER_STRIDE)
+
+        self.conv3 = nn.Conv2d(in_channels=SECOND_LAYER_OUT, out_channels=THIRD_LAYER_OUT,
+                               kernel_size=THIRD_LAYER_KERNEL_SIZE, stride=THIRD_LAYER_STRIDE)
+
+        def conv2d_size_out(size, kernel_size, stride):
+            return (size - (kernel_size - 1) - 1) // stride + 1
+
+        # calculate the output size of the last conv layer
+        conv_width = conv2d_size_out(
+            conv2d_size_out(
+                conv2d_size_out(frame_dim[1], kernel_size=FIRST_LAYER_KERNEL_SIZE, stride=FIRST_LAYER_STRIDE),
+                kernel_size=SECOND_LAYER_KERNEL_SIZE, stride=SECOND_LAYER_STRIDE),
+            kernel_size=THIRD_LAYER_KERNEL_SIZE, stride=THIRD_LAYER_STRIDE)
+        conv_height = conv2d_size_out(
+            conv2d_size_out(
+                conv2d_size_out(frame_dim[0], kernel_size=FIRST_LAYER_KERNEL_SIZE, stride=FIRST_LAYER_STRIDE),
+                kernel_size=SECOND_LAYER_KERNEL_SIZE, stride=SECOND_LAYER_STRIDE),
+            kernel_size=THIRD_LAYER_KERNEL_SIZE, stride=THIRD_LAYER_STRIDE)
+        num_neurons = conv_width * conv_height * THIRD_LAYER_OUT
+        self.fc1 = nn.Linear(in_features=num_neurons, out_features=FC_LAYER)
+
+        # create one output for the critic
+        self.head_critic = nn.Linear(in_features=FC_LAYER, out_features=1)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = F.relu(out)
+
+        out = self.conv2(out)
+        out = F.relu(out)
+
+        out = self.conv3(out)
+        out = F.relu(out)
+
+        out = out.view(out.size()[0], -1)
+
+        out = F.relu(self.fc1(out))
+
+        critic_out = self.head_critic(out)
+
+        return critic_out
