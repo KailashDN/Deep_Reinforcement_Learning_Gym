8104b2bd56ccb27e6446bfcde92cef99f8a80499
Kailash Nadkar <nadkar.k@husky.neu.edu>
2020-11-30 14:34:23 -0500

    CNN model tf1.5

--- Actor_Critic/model.py
+++ Actor_Critic/model.py
@@ -15,3 +15,43 @@ THIRD_LAYER_KERNEL_SIZE = 3
 THIRD_LAYER_STRIDE = 1
 
 FC_LAYER = 1024
+
+
+class ActorCriticNet(nn.Module):
+    def __init__(self, num_actions, frame_dim):
+        super(ActorCriticNet, self).__init__()
+
+        # create a convolution neural network
+        self.conv1 = nn.Conv2d(in_channels=frame_dim[2], out_channels=FIRST_LAYER_OUT,
+                               kernel_size=FIRST_LAYER_KERNEL_SIZE, stride=FIRST_LAYER_STRIDE)
+
+        self.conv2 = nn.Conv2d(in_channels=FIRST_LAYER_OUT, out_channels=SECOND_LAYER_OUT,
+                               kernel_size=SECOND_LAYER_KERNEL_SIZE, stride=SECOND_LAYER_STRIDE)
+
+        self.conv3 = nn.Conv2d(in_channels=SECOND_LAYER_OUT, out_channels=THIRD_LAYER_OUT,
+                               kernel_size=THIRD_LAYER_KERNEL_SIZE, stride=THIRD_LAYER_STRIDE)
+
+        def conv2d_size_out(size, kernel_size, stride):
+            return (size - (kernel_size - 1) - 1) // stride + 1
+
+        # calculate the output size of the last conv layer
+        conv_width = conv2d_size_out(
+            conv2d_size_out(
+                conv2d_size_out(frame_dim[1], kernel_size=FIRST_LAYER_KERNEL_SIZE, stride=FIRST_LAYER_STRIDE),
+                kernel_size=SECOND_LAYER_KERNEL_SIZE, stride=SECOND_LAYER_STRIDE),
+            kernel_size=THIRD_LAYER_KERNEL_SIZE, stride=THIRD_LAYER_STRIDE)
+        conv_height = conv2d_size_out(
+            conv2d_size_out(
+                conv2d_size_out(frame_dim[0], kernel_size=FIRST_LAYER_KERNEL_SIZE, stride=FIRST_LAYER_STRIDE),
+                kernel_size=SECOND_LAYER_KERNEL_SIZE, stride=SECOND_LAYER_STRIDE),
+            kernel_size=THIRD_LAYER_KERNEL_SIZE, stride=THIRD_LAYER_STRIDE)
+        num_neurons = conv_width * conv_height * THIRD_LAYER_OUT
+        self.fc1 = nn.Linear(in_features=num_neurons, out_features=FC_LAYER)
+
+        # create one output for the actor
+        self.head_actor = nn.Linear(in_features=FC_LAYER, out_features=num_actions)
+
+        # create one output for the critic
+        self.head_critic = nn.Linear(in_features=FC_LAYER, out_features=1)
+
+
