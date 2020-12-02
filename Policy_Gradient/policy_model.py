47bbc11442746e057307056dc4f8ff8ffecf87c8
Kailash Nadkar <nadkar.k@husky.neu.edu>
2020-12-02 00:02:43 -0500

    model

--- Policy_Gradient/policy_model.py
+++ Policy_Gradient/policy_model.py
@@ -30,7 +30,36 @@ class Policy(nn.Module):
                                kernel_size=THIRD_LAYER_KERNEL_SIZE, stride=THIRD_LAYER_STRIDE)
         self.conv3_bn = nn.BatchNorm2d(THIRD_LAYER_OUT)
 
-    def conv2d_size_out(size, kernel_size, stride):
-        return (size - (kernel_size - 1) - 1) // stride + 1
+        def conv2d_size_out(size, kernel_size, stride):
+            return (size - (kernel_size - 1) - 1) // stride + 1
 
+        # calculate the output size of the last conv layer
+        conv_width = conv2d_size_out(conv2d_size_out(conv2d_size_out(frame_dim[1], kernel_size=FIRST_LAYER_KERNEL_SIZE,
+                                                                     stride=FIRST_LAYER_STRIDE),
+                                                     kernel_size=SECOND_LAYER_KERNEL_SIZE, stride=SECOND_LAYER_STRIDE),
+                                     kernel_size=THIRD_LAYER_KERNEL_SIZE, stride=THIRD_LAYER_STRIDE)
+        conv_height = conv2d_size_out(conv2d_size_out(conv2d_size_out(frame_dim[0], kernel_size=FIRST_LAYER_KERNEL_SIZE,
+                                                                      stride=FIRST_LAYER_STRIDE),
+                                                      kernel_size=SECOND_LAYER_KERNEL_SIZE, stride=SECOND_LAYER_STRIDE),
+                                      kernel_size=THIRD_LAYER_KERNEL_SIZE, stride=THIRD_LAYER_STRIDE)
+
+        num_neurons = conv_width * conv_height * THIRD_LAYER_OUT
+        self.fc1 = nn.Linear(in_features=num_neurons, out_features=1024)
+        self.fc2 = nn.Linear(in_features=1024, out_features=num_actions)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = F.elu(self.conv1_bn(out))
+
+        out = self.conv2(out)
+        out = F.elu(self.conv2_bn(out))
+
+        out = self.conv3(out)
+        out = F.elu(self.conv3_bn(out))
+        out = out.view(out.size()[0], -1)
+
+        out = F.elu(self.fc1(out))
+        out = F.softmax(self.fc2(out), dim=-1)
+
+        return out
 
