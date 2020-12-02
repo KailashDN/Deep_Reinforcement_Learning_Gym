4721bd79fa237f6681cda9059492f5ffe2274af5
Kailash Nadkar <nadkar.k@husky.neu.edu>
2020-12-01 23:59:33 -0500

    policy gradient CNN model

--- Policy_Gradient/policy_model.py
+++ Policy_Gradient/policy_model.py
@@ -13,3 +13,19 @@ THIRD_LAYER_KERNEL_SIZE = 3
 THIRD_LAYER_STRIDE = 1
 THIRD_LAYER_OUT = 32
 
+
+class Policy(nn.Module):
+    def __init__(self, num_actions, frame_dim):
+        super(Policy, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=frame_dim[2], out_channels=FIRST_LAYER_OUT,
+                               kernel_size=FIRST_LAYER_KERNEL_SIZE, stride=FIRST_LAYER_STRIDE)
+        self.conv1_bn = nn.BatchNorm2d(FIRST_LAYER_OUT)
+
+        self.conv2 = nn.Conv2d(in_channels=FIRST_LAYER_OUT, out_channels=SECOND_LAYER_OUT,
+                               kernel_size=SECOND_LAYER_KERNEL_SIZE, stride=SECOND_LAYER_STRIDE)
+        self.conv2_bn = nn.BatchNorm2d(SECOND_LAYER_OUT)
+
+        self.conv3 = nn.Conv2d(in_channels=SECOND_LAYER_OUT, out_channels=THIRD_LAYER_OUT,
+                               kernel_size=THIRD_LAYER_KERNEL_SIZE, stride=THIRD_LAYER_STRIDE)
+        self.conv3_bn = nn.BatchNorm2d(THIRD_LAYER_OUT)
